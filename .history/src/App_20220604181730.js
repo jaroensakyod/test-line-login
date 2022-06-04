94dcfd031602b0ef0d256d9a2dce6349570e2683
jaroensak <jaroensk.yod@gmail.com>
2022-06-04 18:18:16 +0700

    chaing liff id
    Signed-off-by: jaroensak <jaroensk.yod@gmail.com>

--- .history/src/App_20220604181730.js
+++ .history/src/App_20220604181730.js
@@ -17,7 +17,7 @@ function App() {
   }
 
   const initLine = () => {
-    liff.init({ liffId: '1657190472-QMekRe4A' }, () => {
+    liff.init({ liffId: '1655665373-YAopzeO6' }, () => {
       if (liff.isLoggedIn()) {
         runApp();
       } else {
